d9125609af8e392e2475c11eb0159f8fa98ae8ab
Dakhadil <davitikhadilashvili@gmail.com>
2026-04-27 19:27:10 +0200

    main de la app

---
+++ src/main/java/com/tt1/simulacion/SimulacionApplication.java
@@ -0,0 +1,12 @@
+package com.tt1.simulacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimulacionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimulacionApplication.class, args);
+    }
+}
